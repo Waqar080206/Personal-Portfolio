b382f4f2823130dfc66d3aa56a7d57e2bb631957
Waqar Akhtar <syed.waqar.akhtar08@gmail.com>
2025-11-21 21:13:44 +0530

    F

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Add this to help with ES modules
-  experimental: {
-    esmExternals: 'loose'
-  }
 };
 
-// Use ES module export
-export default nextConfig;
-
-// OR use CommonJS (pick one, not both)
-// module.exports = nextConfig;
+module.exports = nextConfig;
